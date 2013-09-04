c949785725067399997c07d5f1adb16993efbab0
Ricardo Alcocer <alco@ricardoalcocer.com>
2013-09-04 11:20:18 -0700

    Removed unused instances of the class

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,58 +1,40 @@
+var ABH,actionBarHelper;
+
 function doopen(e){
-	var ABH=require('actionbarhelper').actionBarHelper;
-	var actionBarHelper=new ABH($.index);
+	// we set the library only when the activity is available
+	ABH=require('actionbarhelper').actionBarHelper;
+	actionBarHelper=new ABH($.index);
 
 	actionBarHelper.setTitle('Alco Testing 123');
 }
 
 function changeTitle(){
-	var ABH=require('actionbarhelper').actionBarHelper;
-	var actionBarHelper=new ABH($.index);
-
 	actionBarHelper.setTitle('This is the new title');
 }
 
 function setUpButtonUp(){
-	var ABH=require('actionbarhelper').actionBarHelper;
-	var actionBarHelper=new ABH($.index);	
-
 	actionBarHelper.setUpAction(function(){
 		alert('Hey, stop clicking!');
 	})
 }
 
 function setUpButtonDn(){
-	var ABH=require('actionbarhelper').actionBarHelper;
-	var actionBarHelper=new ABH($.index);	
-
 	actionBarHelper.setUpAction();
 }
 
 function setIcon(){
-	var ABH=require('actionbarhelper').actionBarHelper;
-	var actionBarHelper=new ABH($.index);	
-
 	actionBarHelper.setIcon('/appicon2.png');
 }
 
 function setBackground(){
-	var ABH=require('actionbarhelper').actionBarHelper;
-	var actionBarHelper=new ABH($.index);	
-
 	actionBarHelper.setBackgroundImage('/appicon2.png');
 }
 
 function show(){
-	var ABH=require('actionbarhelper').actionBarHelper;
-	var actionBarHelper=new ABH($.index);	
-
 	actionBarHelper.show();
 }
 
 function hide(){
-	var ABH=require('actionbarhelper').actionBarHelper;
-	var actionBarHelper=new ABH($.index);	
-
 	actionBarHelper.hide();
 }
 
